9365434ca0044394732d57c77c5b4f78088250a0
Momen <abdulmomen.official@gmail.com>
2024-11-21 03:53:33 +0600

    requirements.txt updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "hate-speech-classification",
+    version="0.0.1",
+    author="momen",
+    author_email="abdulmomen.official@gmali.com",
+    packages=find_packages(),
+    install_requirements =[],
+
+)
