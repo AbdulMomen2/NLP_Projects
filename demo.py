c3007ae3fece3a88d1b9d5b84a6a982574927619
Momen <abdulmomen.official@gmail.com>
2024-11-24 01:51:37 +0600

    exception updated

--- demo.py
+++ demo.py
@@ -1,3 +1,13 @@
 from hate.logger import logging
+import sys
+from hate.exception import CustomException
 
-logging.info("Welcome")
+# try:
+#     # Example code that causes an error
+#     1 / "0"
+# except Exception as e:
+#     # Raise a custom exception
+#     raise CustomException("An error occurred", sys)
+
+
+#logging.info("Welcome")
